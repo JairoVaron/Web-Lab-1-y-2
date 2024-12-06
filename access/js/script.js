11ba10e61a085fbf581552217ee9e66ba8995682
JairoVaron <jairovaron404@gmail.com>
2024-12-06 15:00:35 -0500

    nueva actualizacion

---
+++ access/js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contacFormulario').addEventListener('submit',function(event){
+    event.preventDefault();
+    alert('Formulario enviado correctamenta. Gracias por contactarnos.');
+});
